0ebb6bd0b350e50f0b437ebf23cdb82851eda99c
myusernamep-cardozo <contato.pcardozo@gmail.com>
2020-09-18 13:57:17 -0300

    implementando tooltip

--- dont-waste/ProximoAcessoViewController.swift
+++ dont-waste/ProximoAcessoViewController.swift
@@ -9,42 +9,58 @@
 import UIKit
 
 
-class ProximoAcessoViewController: UIViewController {
+class ProximoAcessoViewController: UIViewController{
 
     @IBOutlet weak var botaoProximo: UIButton!
     
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var senhaTextField: UITextField!
+    @IBOutlet weak var dicaLabel: UILabel!
+    
     
     override func viewDidLoad() {
+        dicaLabel.isHidden = true
         super.viewDidLoad()
         self.botaoProximo.layer.borderWidth = 1
         self.botaoProximo.layer.borderColor = UIColor.black.cgColor
+        
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        self.view.addGestureRecognizer(tap)
     }
+    
+    var timer: Timer?
     
   //botao casa - para retornar para a pagina home
     @IBAction func botaoCasa(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
     }
     
    //botao dica - colocar dica da senha
     @IBAction func botaoTooltip(_ sender: Any) {
+        DispatchQueue.main.asyncAfter(deadline: .now()) {
+                self.dicaLabel.isHidden = false
+            }
+        timer = Timer.scheduledTimer (timeInterval: 5.0, target: self, selector: #selector (timerAction), userInfo: nil, repeats: false)
     }
     
+    @objc func timerAction () {
+        dicaLabel.isHidden = true
+        }
     
     @IBAction func botaoProximo(_ sender: Any) {
         if emailTextField.text == "" || senhaTextField.text == ""{
-             mostraAlerta()
-        } else if emailTextField.text!.validaEmail && senhaTextField.text!.validaSenha {
+             mostraAlerta(mensagem: "Preencha todos os campos")
+        } else
+            if emailTextField.text!.validaEmail && senhaTextField.text!.validaSenha {
             print("tudo certo")
         }else {
-           print("algum campo esta errado")
+           mostraAlerta(mensagem: "Algum campo esta inválido")
         }
     }
     
-    
-    // popup alerta caso os campos estiverem vazios
-    func mostraAlerta(){
-    let alert = UIAlertController(title: "Atenção", message: "Preencha todos os campos", preferredStyle: UIAlertController.Style.alert)
+    // popup alerta caso os campos estiverem vazios ou invalidos
+    func mostraAlerta(mensagem: String){
+    let alert = UIAlertController(title: "Atenção", message: mensagem, preferredStyle: UIAlertController.Style.alert)
     alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
     self.present(alert, animated: true, completion: nil)
     }
@@ -65,6 +81,10 @@ extension String {
        
     }
 }
+
+
+    
+   
 
 
 
